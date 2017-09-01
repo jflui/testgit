6de56b8dd36af160201dc53855cc8c58627b3d9c
JF <jflui@yahoo.com>
2017-09-01 16:54:39 +0800

    Initial Commit

---
+++ gittest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest2
+//
+//  Created by jinfoong on 1/9/17.
+//  Copyright © 2017 jinfoong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
